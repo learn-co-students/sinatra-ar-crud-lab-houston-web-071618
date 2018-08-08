ae6842f4cf2a212a7b9be27d77b5a9fe63ef1c77
Alejandro Aguilar Jr <alejandro.aguilar.jr@gmail.com>
2018-08-07 22:50:41 -0500

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,9 +6,42 @@ class ApplicationController < Sinatra::Base
   configure do
     set :public_folder, 'public'
     set :views, 'app/views'
+     set :method_override, true
   end
 
-  get '/' do
+  get '/posts/new' do
+    erb :new
+  end
 
+  post '/posts' do  #creates a post
+  @post = Post.create(params)
+  redirect to '/posts'
+  end
+
+  get '/posts' do
+    @posts = Post.all
+    erb :index
+  end
+
+  get '/posts/:id' do
+    @post = Post.find(params[:id])
+    erb :show
+  end
+
+  get '/posts/:id/edit' do
+    @post = Post.find_by_id(params[:id])
+    erb :edit
+  end
+
+  patch '/posts/:id' do
+    post = Post.find(params[:id])
+    post.update(params[:post])
+    redirect "/posts/#{params[:id]}"
+  end
+
+  delete '/posts/:id/delete' do
+    @post = Post.find(params[:id])
+    @post.delete
+    erb :delete
   end
 end
