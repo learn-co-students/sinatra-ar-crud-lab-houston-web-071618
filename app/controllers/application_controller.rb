d86ccad30b2f0c0253513cbc72e59322b0756143
Marylene Chan <marylenechan@Marylenes-MacBook-Pro-2.local>
2018-08-08 20:20:53 -0500

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,6 +9,50 @@ class ApplicationController < Sinatra::Base
   end
 
   get '/' do
-
+    erb :index
   end
+
+  get '/posts/new' do
+    erb :new
+  end
+
+  def posts
+    Post.all
+  end
+
+  get '/posts' do
+    erb :index
+  end
+
+  get '/posts/:id' do
+    @post = Post.find(params[:id])
+    erb :show
+  end
+
+  get '/posts/:id/edit' do
+    @post = Post.find(params[:id])
+    erb :edit
+  end
+
+  post '/posts' do
+    Post.create(params[:post])
+    # binding.pry
+    erb :index
+  end
+
+  patch '/posts/:id' do
+    post = Post.find(params[:id])
+
+    post.update(params[:post])
+
+    redirect "/posts/#{params[:id]}"
+  end
+
+  delete '/posts/:id/delete' do
+    Post.destroy(params[:id])
+
+    # redirect "/posts/#{params[:id]}"
+    erb :delete
+  end
+
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,10 @@
 #Placeholder for a model
 
 class Post < ActiveRecord::Base
+  # def initialize(params)
+  #   @name = params[:name]
+  #   @content = params[:content]
+  # end
+
+
 end
